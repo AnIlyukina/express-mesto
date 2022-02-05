d78feeab93ac8947183ea8e3bcb95d3fc42f32ce
AnnaIluykina <anyailyukina@yandex.ru>
2022-02-05 23:06:03 +0800

    Написать Котроллеры для Users

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,33 @@
 const User = require('../models/user');
 
-exports.getUsers = async (res) => {
-  const users = await User.find({});
-  res.status(200).send(users);
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(500).send({ message: 'Произошла ошибка' });
+  }
 };
 
 exports.getUsersById = async (req, res) => {
-  const user = await User.findById(req.params.id);
-  res.status(200).send(user);
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.status(200).send(user);
+    } else {
+      res.status(404).send({ message: 'Пользователь не найден' });
+    }
+  // eslint-disable-next-line no-empty
+  } catch (err) {
+    res.status(500).send({ message: 'Произошла ошибка' });
+  }
 };
 
-// exports.createUsers = (req, res) => {
-//   res.status(201).send(req.body);
-// };
+exports.createUsers = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    res.status(201).send(await user.save());
+  } catch (err) {
+    res.status(500).send({ message: 'Произошла ошибка' });
+  }
+};
