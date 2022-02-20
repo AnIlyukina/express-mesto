5a03e356f5b6af7e525bc76509e2513d2f824eef
AnnaIluykina <anyailyukina@yandex.ru>
2022-02-20 12:57:14 +0800

    Централизованная обработка ошибок cards.js(контроллеры)

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,15 +1,16 @@
+const { BadRequestError, ForbiddenError } = require('../errors');
 const Card = require('../models/card');
 
-exports.getCards = async (req, res) => {
+exports.getCards = async (req, res, next) => {
   try {
     const cards = await Card.find({});
     res.status(200).send(cards);
   } catch (err) {
-    res.status(500).send({ message: 'Произошла ошибка на сервере' });
+    next(err);
   }
 };
 
-exports.createCard = async (req, res) => {
+exports.createCard = async (req, res, next) => {
   try {
     const card = new Card({
       name: req.body.name,
@@ -19,31 +20,29 @@ exports.createCard = async (req, res) => {
     res.status(201).send(await card.save());
   } catch (err) {
     if (err.name === 'ValidationError') {
-      res.status(400).send({ message: 'Переданы невалидные данные' });
-    } else {
-      res.status(500).send({ message: 'Произошла ошибка на сервере' });
+      next(new BadRequestError('Переданы невалидные данные'));
     }
+    next(err);
   }
 };
 
-exports.deleteCard = async (req, res) => {
+exports.deleteCard = async (req, res, next) => {
   try {
     const card = await Card.findByIdAndDelete(req.params.cardId);
     if (card) {
       res.status(200).send(card);
     } else {
-      res.status(404).send({ message: 'Пользователь не найден' });
+      throw new ForbiddenError('Пользователь не найден');
     }
   } catch (err) {
     if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Невалидный id ' });
-    } else {
-      res.status(500).send({ message: 'Произошла ошибка на сервере' });
+      next(new BadRequestError('Переданы невалидные данные'));
     }
+    next(err);
   }
 };
 
-exports.likeCard = async (req, res) => {
+exports.likeCard = async (req, res, next) => {
   try {
     const cardLike = await Card.findByIdAndUpdate(
       req.params.cardId,
@@ -53,18 +52,17 @@ exports.likeCard = async (req, res) => {
     if (cardLike) {
       res.status(200).send(cardLike);
     } else {
-      res.status(404).send({ message: 'Пользователь не найден' });
+      throw new ForbiddenError('Пользователь не найден');
     }
   } catch (err) {
     if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Невалидный id ' });
-    } else {
-      res.status(500).send({ message: 'Произошла ошибка на сервере' });
+      next(new BadRequestError('Переданы невалидные данные'));
     }
+    next(err);
   }
 };
 
-exports.dislikeCard = async (req, res) => {
+exports.dislikeCard = async (req, res, next) => {
   try {
     const cardDislike = await Card.findByIdAndUpdate(
       req.params.cardId,
@@ -74,13 +72,12 @@ exports.dislikeCard = async (req, res) => {
     if (cardDislike) {
       res.status(200).send(cardDislike);
     } else {
-      res.status(404).send({ message: 'Пользователь не найден' });
+      throw new ForbiddenError('Пользователь не найден');
     }
   } catch (err) {
     if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Невалидный id ' });
-    } else {
-      res.status(500).send({ message: 'Произошла ошибка на сервере' });
+      next(new BadRequestError('Переданы невалидные данные'));
     }
+    next(err);
   }
 };
